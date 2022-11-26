ad880bbd45639dcead0eb790a3418f05363e2603
Stephen Hidem <StephenHidem@outlook.com>
2022-11-26 12:40:27 -0600

    Added AverageCadence to StandardCrankTorqueSensor class.

--- AntPlus/DeviceProfiles/StandardCrankTorqueSensor.cs
+++ AntPlus/DeviceProfiles/StandardCrankTorqueSensor.cs
@@ -2,5 +2,18 @@
 {
     public class StandardCrankTorqueSensor : TorqueSensor
     {
+        /// <summary>Gets the average cadence in rotations per minute.</summary>
+        /// <value>The average cadence.</value>
+        public double AverageCadence { get; private set; }
+
+        public override void Parse(byte[] dataPage)
+        {
+            bool firstPage = isFirstDataMessage; // save first message flag for later use
+            base.Parse(dataPage);
+            if (!firstPage)
+            {
+                AverageCadence = 60.0 * deltaEventCount / (deltaPeriod / 2048.0);
+            }
+        }
     }
 }

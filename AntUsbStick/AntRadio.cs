e396b65273191c5d8350c9e8165a5b88a215fd98
Stephen Hidem <StephenHidem@outlook.com>
2023-01-20 17:08:13 -0600

    Using expression bodies.

--- AntUsbStick/AntRadio.cs
+++ AntUsbStick/AntRadio.cs
@@ -37,10 +37,7 @@ namespace SmallEarthTech.AntUsbStick
         public uint SerialNumber => antDevice.getSerialNumber();
 
         /// <inheritdoc/>
-        public void CancelTransfers(int cancelWaitTime)
-        {
-            antDevice.cancelTransfers(cancelWaitTime);
-        }
+        public void CancelTransfers(int cancelWaitTime) => antDevice.cancelTransfers(cancelWaitTime);
 
         /// <summary>Initializes a new instance of the <see cref="AntRadio" /> class.</summary>
         public AntRadio()
@@ -55,82 +52,43 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public void ConfigureAdvancedBursting(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields)
-        {
-            antDevice.configureAdvancedBursting(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields);
-        }
+        public void ConfigureAdvancedBursting(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields) => antDevice.configureAdvancedBursting(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields);
 
         /// <inheritdoc/>
-        public bool ConfigureAdvancedBursting(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields, uint responseWaitTime)
-        {
-            return antDevice.configureAdvancedBursting(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields, responseWaitTime);
-        }
+        public bool ConfigureAdvancedBursting(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields, uint responseWaitTime) => antDevice.configureAdvancedBursting(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields, responseWaitTime);
 
         /// <inheritdoc/>
-        public void ConfigureAdvancedBursting_ext(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields, ushort stallCount, byte retryCount)
-        {
-            antDevice.configureAdvancedBursting_ext(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields, stallCount, retryCount);
-        }
+        public void ConfigureAdvancedBursting_ext(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields, ushort stallCount, byte retryCount) => antDevice.configureAdvancedBursting_ext(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields, stallCount, retryCount);
 
         /// <inheritdoc/>
-        public bool ConfigureAdvancedBursting_ext(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields, ushort stallCount, byte retryCount, uint responseWaitTime)
-        {
-            return antDevice.configureAdvancedBursting_ext(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields, stallCount, retryCount, responseWaitTime);
-        }
+        public bool ConfigureAdvancedBursting_ext(bool enable, byte maxPacketLength, AdvancedBurstConfigFlags requiredFields, AdvancedBurstConfigFlags optionalFields, ushort stallCount, byte retryCount, uint responseWaitTime) => antDevice.configureAdvancedBursting_ext(enable, maxPacketLength, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)requiredFields, (ANT_ReferenceLibrary.AdvancedBurstConfigFlags)optionalFields, stallCount, retryCount, responseWaitTime);
 
         /// <inheritdoc/>
-        public bool ConfigureAdvancedBurstSplitting(bool splitBursts)
-        {
-            return antDevice.configureAdvancedBurstSplitting(splitBursts);
-        }
+        public bool ConfigureAdvancedBurstSplitting(bool splitBursts) => antDevice.configureAdvancedBurstSplitting(splitBursts);
 
         /// <inheritdoc/>
-        public void ConfigureEventBuffer(EventBufferConfig config, ushort size, ushort time)
-        {
-            antDevice.configureEventBuffer((ANT_ReferenceLibrary.EventBufferConfig)config, size, time);
-        }
+        public void ConfigureEventBuffer(EventBufferConfig config, ushort size, ushort time) => antDevice.configureEventBuffer((ANT_ReferenceLibrary.EventBufferConfig)config, size, time);
 
         /// <inheritdoc/>
-        public bool ConfigureEventBuffer(EventBufferConfig config, ushort size, ushort time, uint responseWaitTime)
-        {
-            return antDevice.configureEventBuffer((ANT_ReferenceLibrary.EventBufferConfig)config, size, time, responseWaitTime);
-        }
+        public bool ConfigureEventBuffer(EventBufferConfig config, ushort size, ushort time, uint responseWaitTime) => antDevice.configureEventBuffer((ANT_ReferenceLibrary.EventBufferConfig)config, size, time, responseWaitTime);
 
         /// <inheritdoc/>
-        public void ConfigureEventFilter(ushort eventFilter)
-        {
-            antDevice.configureEventFilter(eventFilter);
-        }
+        public void ConfigureEventFilter(ushort eventFilter) => antDevice.configureEventFilter(eventFilter);
 
         /// <inheritdoc/>
-        public bool ConfigureEventFilter(ushort eventFilter, uint responseWaitTime)
-        {
-            return antDevice.configureEventFilter(eventFilter, responseWaitTime);
-        }
+        public bool ConfigureEventFilter(ushort eventFilter, uint responseWaitTime) => antDevice.configureEventFilter(eventFilter, responseWaitTime);
 
         /// <inheritdoc/>
-        public void ConfigureHighDutySearch(bool enable, byte suppressionCycles)
-        {
-            antDevice.configureHighDutySearch(enable, suppressionCycles);
-        }
+        public void ConfigureHighDutySearch(bool enable, byte suppressionCycles) => antDevice.configureHighDutySearch(enable, suppressionCycles);
 
         /// <inheritdoc/>
-        public bool ConfigureHighDutySearch(bool enable, byte suppressionCycles, uint responseWaitTime)
-        {
-            return antDevice.configureHighDutySearch(enable, suppressionCycles, responseWaitTime);
-        }
+        public bool ConfigureHighDutySearch(bool enable, byte suppressionCycles, uint responseWaitTime) => antDevice.configureHighDutySearch(enable, suppressionCycles, responseWaitTime);
 
         /// <inheritdoc/>
-        public void ConfigureUserNvm(ushort address, byte[] data, byte size)
-        {
-            antDevice.configureUserNvm(address, data, size);
-        }
+        public void ConfigureUserNvm(ushort address, byte[] data, byte size) => antDevice.configureUserNvm(address, data, size);
 
         /// <inheritdoc/>
-        public bool ConfigureUserNvm(ushort address, byte[] data, byte size, uint responseWaitTime)
-        {
-            return antDevice.configureUserNvm(address, data, size, responseWaitTime);
-        }
+        public bool ConfigureUserNvm(ushort address, byte[] data, byte size, uint responseWaitTime) => antDevice.configureUserNvm(address, data, size, responseWaitTime);
 
         /// <inheritdoc/>
         public void CryptoKeyNVMOp(EncryptionNVMOp memoryOperation, byte nonVolatileKeyIndex, byte[] operationData)
@@ -145,16 +103,10 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public void CrystalEnable()
-        {
-            antDevice.crystalEnable();
-        }
+        public void CrystalEnable() => antDevice.crystalEnable();
 
         /// <inheritdoc/>
-        public bool CrystalEnable(uint responseWaitTime)
-        {
-            return antDevice.crystalEnable(responseWaitTime);
-        }
+        public bool CrystalEnable(uint responseWaitTime) => antDevice.crystalEnable(responseWaitTime);
 
         /// <inheritdoc/>
         public void Dispose()
@@ -163,64 +115,34 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public void EnableLED(bool isEnabled)
-        {
-            antDevice.EnableLED(isEnabled);
-        }
+        public void EnableLED(bool isEnabled) => antDevice.EnableLED(isEnabled);
 
         /// <inheritdoc/>
-        public bool EnableLED(bool isEnabled, uint responseWaitTime)
-        {
-            return antDevice.EnableLED(isEnabled, responseWaitTime);
-        }
+        public bool EnableLED(bool isEnabled, uint responseWaitTime) => antDevice.EnableLED(isEnabled, responseWaitTime);
 
         /// <inheritdoc/>
-        public void EnableRxExtendedMessages(bool isEnabled)
-        {
-            antDevice.enableRxExtendedMessages(isEnabled);
-        }
+        public void EnableRxExtendedMessages(bool isEnabled) => antDevice.enableRxExtendedMessages(isEnabled);
 
         /// <inheritdoc/>
-        public bool EnableRxExtendedMessages(bool isEnabled, uint responseWaitTime)
-        {
-            return antDevice.enableRxExtendedMessages(isEnabled, responseWaitTime);
-        }
+        public bool EnableRxExtendedMessages(bool isEnabled, uint responseWaitTime) => antDevice.enableRxExtendedMessages(isEnabled, responseWaitTime);
 
         /// <inheritdoc/>
-        public void FitAdjustPairingSettings(byte searchLv, byte pairLv, byte trackLv)
-        {
-            antDevice.fitAdjustPairingSettings(searchLv, pairLv, trackLv);
-        }
+        public void FitAdjustPairingSettings(byte searchLv, byte pairLv, byte trackLv) => antDevice.fitAdjustPairingSettings(searchLv, pairLv, trackLv);
 
         /// <inheritdoc/>
-        public bool FitAdjustPairingSettings(byte searchLv, byte pairLv, byte trackLv, uint responseWaitTime)
-        {
-            return antDevice.fitAdjustPairingSettings(searchLv, pairLv, trackLv, responseWaitTime);
-        }
+        public bool FitAdjustPairingSettings(byte searchLv, byte pairLv, byte trackLv, uint responseWaitTime) => antDevice.fitAdjustPairingSettings(searchLv, pairLv, trackLv, responseWaitTime);
 
         /// <inheritdoc/>
-        public void FitSetFEState(byte feState)
-        {
-            antDevice.fitSetFEState(feState);
-        }
+        public void FitSetFEState(byte feState) => antDevice.fitSetFEState(feState);
 
         /// <inheritdoc/>
-        public bool FitSetFEState(byte feState, uint responseWaitTime)
-        {
-            return antDevice.fitSetFEState(feState, responseWaitTime);
-        }
+        public bool FitSetFEState(byte feState, uint responseWaitTime) => antDevice.fitSetFEState(feState, responseWaitTime);
 
         /// <inheritdoc/>
-        public IAntChannel GetChannel(int num)
-        {
-            return new AntChannel(antDevice.getChannel(num));
-        }
+        public IAntChannel GetChannel(int num) => new AntChannel(antDevice.getChannel(num));
 
         /// <inheritdoc/>
-        public IDeviceCapabilities GetDeviceCapabilities()
-        {
-            return (IDeviceCapabilities)antDevice.getDeviceCapabilities();
-        }
+        public IDeviceCapabilities GetDeviceCapabilities() => (IDeviceCapabilities)antDevice.getDeviceCapabilities();
 
         /// <inheritdoc/>
         public IDeviceCapabilities GetDeviceCapabilities(bool forceNewCopy, uint responseWaitTime)
@@ -249,52 +171,28 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public void LoadCryptoKeyFromNVM(byte nonVolatileKeyIndex, byte volatileKeyIndex)
-        {
-            antDevice.loadCryptoKeyFromNVM(nonVolatileKeyIndex, volatileKeyIndex);
-        }
+        public void LoadCryptoKeyFromNVM(byte nonVolatileKeyIndex, byte volatileKeyIndex) => antDevice.loadCryptoKeyFromNVM(nonVolatileKeyIndex, volatileKeyIndex);
 
         /// <inheritdoc/>
-        public bool LoadCryptoKeyFromNVM(byte nonVolatileKeyIndex, byte volatileKeyIndex, uint responseWaitTime)
-        {
-            return antDevice.loadCryptoKeyFromNVM(nonVolatileKeyIndex, volatileKeyIndex, responseWaitTime);
-        }
+        public bool LoadCryptoKeyFromNVM(byte nonVolatileKeyIndex, byte volatileKeyIndex, uint responseWaitTime) => antDevice.loadCryptoKeyFromNVM(nonVolatileKeyIndex, volatileKeyIndex, responseWaitTime);
 
         /// <inheritdoc/>
-        public void OpenRxScanMode()
-        {
-            antDevice.openRxScanMode();
-        }
+        public void OpenRxScanMode() => antDevice.openRxScanMode();
 
         /// <inheritdoc/>
-        public bool OpenRxScanMode(uint responseWaitTime)
-        {
-            return antDevice.openRxScanMode(responseWaitTime);
-        }
+        public bool OpenRxScanMode(uint responseWaitTime) => antDevice.openRxScanMode(responseWaitTime);
 
         /// <inheritdoc/>
-        public IAntResponse ReadUserNvm(ushort address, byte size)
-        {
-            return new AntResponse(antDevice.readUserNvm(address, size));
-        }
+        public IAntResponse ReadUserNvm(ushort address, byte size) => new AntResponse(antDevice.readUserNvm(address, size));
 
         /// <inheritdoc/>
-        public IAntResponse ReadUserNvm(ushort address, byte size, uint responseWaitTime)
-        {
-            return new AntResponse(antDevice.readUserNvm(address, size, responseWaitTime));
-        }
+        public IAntResponse ReadUserNvm(ushort address, byte size, uint responseWaitTime) => new AntResponse(antDevice.readUserNvm(address, size, responseWaitTime));
 
         /// <inheritdoc/>
-        public void RequestMessage(byte channelNum, RequestMessageID messageID)
-        {
-            antDevice.requestMessage(channelNum, (ANT_ReferenceLibrary.RequestMessageID)messageID);
-        }
+        public void RequestMessage(byte channelNum, RequestMessageID messageID) => antDevice.requestMessage(channelNum, (ANT_ReferenceLibrary.RequestMessageID)messageID);
 
         /// <inheritdoc/>
-        public void RequestMessage(RequestMessageID messageID)
-        {
-            antDevice.requestMessage((ANT_ReferenceLibrary.RequestMessageID)messageID);
-        }
+        public void RequestMessage(RequestMessageID messageID) => antDevice.requestMessage((ANT_ReferenceLibrary.RequestMessageID)messageID);
 
         /// <inheritdoc/>
         public IAntResponse RequestMessageAndResponse(byte channelNum, RequestMessageID messageID, uint responseWaitTime)
@@ -309,28 +207,16 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public void ResetSystem()
-        {
-            antDevice.ResetSystem();
-        }
+        public void ResetSystem() => antDevice.ResetSystem();
 
         /// <inheritdoc/>
-        public bool ResetSystem(uint responseWaitTime)
-        {
-            return antDevice.ResetSystem(responseWaitTime);
-        }
+        public bool ResetSystem(uint responseWaitTime) => antDevice.ResetSystem(responseWaitTime);
 
         /// <inheritdoc/>
-        public void ResetUSB()
-        {
-            antDevice.ResetUSB();
-        }
+        public void ResetUSB() => antDevice.ResetUSB();
 
         /// <inheritdoc/>
-        public void SetCryptoID(byte[] encryptionID)
-        {
-            antDevice.setCryptoID(encryptionID);
-        }
+        public void SetCryptoID(byte[] encryptionID) => antDevice.setCryptoID(encryptionID);
 
         /// <inheritdoc/>
         public bool SetCryptoID(byte[] encryptionID, uint responseWaitTime)
@@ -399,16 +285,10 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public void SetNetworkKey(byte netNumber, byte[] networkKey)
-        {
-            antDevice.setNetworkKey(netNumber, networkKey);
-        }
+        public void SetNetworkKey(byte netNumber, byte[] networkKey) => antDevice.setNetworkKey(netNumber, networkKey);
 
         /// <inheritdoc/>
-        public bool SetNetworkKey(byte netNumber, byte[] networkKey, uint responseWaitTime)
-        {
-            return antDevice.setNetworkKey(netNumber, networkKey, responseWaitTime);
-        }
+        public bool SetNetworkKey(byte netNumber, byte[] networkKey, uint responseWaitTime) => antDevice.setNetworkKey(netNumber, networkKey, responseWaitTime);
 
         /// <inheritdoc/>
         public void SetTransmitPowerForAllChannels(TransmitPower transmitPower)
@@ -435,9 +315,6 @@ namespace SmallEarthTech.AntUsbStick
         }
 
         /// <inheritdoc/>
-        public bool WriteRawMessageToDevice(byte msgID, byte[] msgData)
-        {
-            return antDevice.writeRawMessageToDevice(msgID, msgData);
-        }
+        public bool WriteRawMessageToDevice(byte msgID, byte[] msgData) => antDevice.writeRawMessageToDevice(msgID, msgData);
     }
 }

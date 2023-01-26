2cce0d81db330e685f03728d4231ea651c767815
Stephen Hidem <StephenHidem@outlook.com>
2023-01-26 09:31:47 -0600

    Updated device info to provide strings from the underlying class.

--- AntRadioInterface/DeviceInfo.cs
+++ AntRadioInterface/DeviceInfo.cs
@@ -1,25 +1,28 @@
-﻿namespace SmallEarthTech.AntRadioInterface
+﻿using System.Linq;
+using System.Text;
+
+namespace SmallEarthTech.AntRadioInterface
 {
-    /// <summary>The device information class.</summary>
+    /// <summary>The device information class. It converts the hyte arrays to strings that can be used in applictations.</summary>
     public class DeviceInfo
     {
         /// <summary>
         /// USB Device Product Description
         /// </summary>
-        public byte[] ProductDescription { get; }
+        public string ProductDescription { get; }
 
         /// <summary>
         /// USB Device Serial String
         /// </summary>
-        public byte[] SerialString { get; }
+        public string SerialString { get; }
 
         /// <summary>Initializes a new instance of the <see cref="DeviceInfo" /> class.</summary>
         /// <param name="productDescription">The product description.</param>
         /// <param name="serialString">The serial string.</param>
         public DeviceInfo(byte[] productDescription, byte[] serialString)
         {
-            ProductDescription = productDescription;
-            SerialString = serialString;
+            ProductDescription = Encoding.UTF8.GetString(productDescription.TakeWhile(e => e != 0).ToArray());
+            SerialString = Encoding.UTF8.GetString(serialString.TakeWhile(e => e != 0).ToArray());
         }
     }
 }

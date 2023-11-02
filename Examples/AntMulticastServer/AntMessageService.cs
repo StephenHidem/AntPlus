7ebe9c6a3e212450cd942ebe00520cbb71c18b24
Stephen Hidem <StephenHidem@outlook.com>
2023-11-02 18:28:45 -0500

    Aligned the example more closely to the DI pattern.

---
+++ Examples/AntMulticastServer/AntMessageService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SmallEarthTech.AntRadioInterface;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace AntMulticastServer
+{
+    internal class AntMessageService : BackgroundService
+    {
+        private readonly ILogger<AntMessageService> _logger;
+        private readonly IAntRadio _antRadio;
+        private readonly UdpClient _udpClient;
+        private readonly IPEndPoint _udpEndPoint;
+
+        public AntMessageService(ILogger<AntMessageService> logger, IAntRadio antRadio, UdpClient udpClient)
+        {
+            _logger = logger;
+            _antRadio = antRadio;
+            _udpClient = udpClient;
+
+            // create multicast endpoint to send ANT broadcast data to
+            _udpEndPoint = new(IPAddress.Parse("FF02::1"), 55436);
+            Console.WriteLine(string.Format("Establishing IPv6 endpoint - {0}.", _udpEndPoint.ToString()));
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // configure ANT radio
+            Console.WriteLine("Configuring ANT radio (uses the first USB stick found).");
+            IAntChannel[] channel = _antRadio.InitializeContinuousScanMode();
+            channel[0].ChannelResponse += Channel_ChannelResponse;
+            Console.WriteLine("Up and running!");
+
+            // relay received messages to ANT radio to send to an ANT device
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                UdpReceiveResult result = await _udpClient.ReceiveAsync(stoppingToken);
+                _logger.LogDebug(BitConverter.ToString(result.Buffer));
+                ChannelId channelId = new(BitConverter.ToUInt32(result.Buffer, 0));
+                byte[] msg = result.Buffer.Skip(4).Take(8).ToArray();
+                uint ackWaitTime = BitConverter.ToUInt32(result.Buffer, 12);
+                _ = await channel[1].SendExtAcknowledgedData(channelId, msg, ackWaitTime);
+            }
+
+            // remove handler
+            channel[0].ChannelResponse -= Channel_ChannelResponse;
+        }
+
+        /// <summary>
+        /// Messages received from the ANT radio are serialized and forwarded to the multicast endpoint.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The ANT response.</param>
+        private void Channel_ChannelResponse(object? sender, AntResponse e)
+        {
+            MemoryStream ms = new();
+            DataContractSerializer dcs = new(typeof(AntResponse));
+            dcs.WriteObject(ms, e);
+
+            ms.Position = 0;
+            byte[] txt = Encoding.Default.GetBytes(new StreamReader(ms).ReadToEnd());
+
+            _ = _udpClient.Send(txt, txt.Length, _udpEndPoint);
+        }
+    }
+}
--- Examples/AntMulticastServer/Program.cs
+++ Examples/AntMulticastServer/Program.cs
@@ -1,14 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AntMulticastServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using SmallEarthTech.AntRadioInterface;
 using SmallEarthTech.AntUsbStick;
-using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text;
 
 // opening credits
 Console.WriteLine(string.Format("ANT Multicast Server - Version {0}.", Assembly.GetExecutingAssembly().GetName().Version?.ToString(3)));
@@ -17,64 +15,20 @@ Console.WriteLine("Copyright 2023 Stephen Hidem.");
 // Initialize early, without access to configuration or services
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Debug(outputTemplate:
-        "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}") // + file or centralized logging
+        "[{Timestamp:HH:mm:ss.fff} {Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}") // + file or centralized logging
     .MinimumLevel.Debug()
     .CreateLogger();
 
 // dependency services
-IHost host = Host.CreateDefaultBuilder(Environment.GetCommandLineArgs()).
+IHost host = Host.CreateDefaultBuilder(args).
     UseSerilog().
     ConfigureServices(s =>
     {
+        s.AddHostedService<AntMessageService>();
         s.AddSingleton<IAntRadio, AntRadio>();
+        s.AddSingleton<UdpClient>(sp => new UdpClient(2000, AddressFamily.InterNetworkV6));
     }).
     Build();
 
-// create UDP client
-using UdpClient udpServer = new(2000, AddressFamily.InterNetworkV6);
-
-// create multicast endpoint to send ANT data to
-IPEndPoint endPoint = new(IPAddress.Parse("FF02::1"), 55436);
-Console.WriteLine(string.Format("Establishing IPv6 endpoint - {0}.", endPoint.ToString()));
-
-// create and configure ANT radio
-Console.WriteLine("Configuring ANT radio (uses the first USB stick found).");
-AntRadio antRadio = (AntRadio)host.Services.GetRequiredService<IAntRadio>();
-IAntChannel[] channel = antRadio.InitializeContinuousScanMode();
-channel[0].ChannelResponse += Channel_ChannelResponse;
-
-// create background task to receive UDP directed to this server from any clients
-_ = Task.Run(async () =>
-{
-    using IAntChannel txChannel = antRadio.GetChannel(1);
-    while (true)
-    {
-        UdpReceiveResult result = await udpServer.ReceiveAsync();
-        Log.Debug(BitConverter.ToString(result.Buffer));
-        ChannelId channelId = new(BitConverter.ToUInt32(result.Buffer, 0));
-        byte[] msg = result.Buffer.Skip(4).Take(8).ToArray();
-        uint ackWaitTime = BitConverter.ToUInt32(result.Buffer, 12);
-        _ = await txChannel.SendExtAcknowledgedData(channelId, msg, ackWaitTime);
-    }
-});
-
-Console.WriteLine("Up and running!");
-
-// loop until terminated
-Console.WriteLine("Press Enter to terminate.");
-Console.ReadLine();
-
-// clean up
-channel[0].ChannelResponse -= Channel_ChannelResponse;
-
-void Channel_ChannelResponse(object? sender, AntResponse e)
-{
-    MemoryStream ms = new();
-    DataContractSerializer dcs = new(typeof(AntResponse));
-    dcs.WriteObject(ms, e);
-
-    ms.Position = 0;
-    byte[] txt = Encoding.Default.GetBytes(new StreamReader(ms).ReadToEnd());
-
-    _ = udpServer.Send(txt, txt.Length, endPoint);
-}
+// run until terminated
+host.Run();

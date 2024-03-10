88fa935a0e1ce03eb330b5d15ae578763aa86f3f
Stephen Hidem <93548207+StephenHidem@users.noreply.github.com>
2024-03-09 21:06:55 -0600

    Removed unused greeter service.

--- Examples/MAUI-gRPC/AntGrpcService/Program.cs
+++ Examples/MAUI-gRPC/AntGrpcService/Program.cs
@@ -29,7 +29,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<AntRadioService>();
 app.MapGrpcService<AntChannelService>();
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();

785ae1eeac62475432d0a28972ed31370a87a1ed
Stephen Hidem <93548207+StephenHidem@users.noreply.github.com>
2024-08-02 10:26:10 -0500

    Updating explanation of usage.

--- AntPlus.Extensions.Hosting/TimeoutOptions.cs
+++ AntPlus.Extensions.Hosting/TimeoutOptions.cs
@@ -9,7 +9,7 @@
         /// <remarks>
         /// The application host builder retrieves configuration options from various sources such as the command line or
         /// appsettings.json. If the configuration can't find TimeoutOptions, the collection will default to <see cref="MissedMessages"/>.
-        /// If both <see cref="Timeout"/> and <see cref="MissedMessages"/> are defined Timeout is ignored and MissedMessages is used.
+        /// If both <see cref="Timeout"/> and <see cref="MissedMessages"/> are defined. Timeout is ignored and MissedMessages is used.
         /// Prefer MissedMessages as this will scale the ANT device timeout based on the specific data broadcast rate of an ANT device.
         /// Asset trackers broadcast 16 times a second and Geocaches broadcast once every 2 seconds. Other devices typically
         /// broadcast 4 times a second.
@@ -25,6 +25,7 @@
         /// </code>
         /// The command line could define the timeout "--TimeoutOptions:MissedMessages=8".
         /// </para>
+        /// <para>Timeouts can be disabled by setting Timeout to -1 and ignoring or setting MissedMessages to null.</para>
         /// </remarks>
         public sealed class TimeoutOptions
         {
@@ -32,7 +33,7 @@
             /// Gets or sets the timeout before an ANT device goes offline.
             /// </summary>
             /// <value>
-            /// The timeout in milliseconds.
+            /// The timeout in milliseconds. Set to -1 to disable device timeout.
             /// </value>
             public int? Timeout { get; set; }
             /// <summary>

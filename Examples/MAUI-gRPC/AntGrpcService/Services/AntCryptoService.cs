2f24ad03d5faab073a890887828870c62fd4ed89
Stephen Hidem <StephenHidem@outlook.com>
2025-03-06 14:52:43 -0600

    Add AntCryptoService for cryptographic operations
    This commit introduces a new gRPC service, `AntCryptoService`, which provides methods for managing cryptographic keys and settings, including loading keys from non-volatile memory and setting cryptographic IDs.
    
    The service is registered in `Program.cs`, and the project file `AntGrpcShared.csproj` is updated to include the new `ant_crypto.proto` file that defines the service and its methods.
    
    Additionally, `AntRadioService.cs` is modified to include a client for the new service, and a new file `AntCryptoService.cs` is created to implement the defined methods.
    
    These changes enhance the application's functionality by adding support for cryptographic operations.

---
+++ Examples/MAUI-gRPC/AntGrpcService/Services/AntCryptoService.cs
@@ -0,0 +1,60 @@
+﻿using AntCryptoGrpcService;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using SmallEarthTech.AntRadioInterface;
+
+namespace AntGrpcService.Services
+{
+    public class AntCryptoService(IAntRadio antRadio) : gRPCAntCrypto.gRPCAntCryptoBase
+    {
+        private readonly ICrypto _crypto = (ICrypto)antRadio;
+
+        public override Task<BoolValue> CryptoKeyNVMOp(CryptoKeyNVMOpRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.CryptoKeyNVMOp((SmallEarthTech.AntRadioInterface.EncryptionNVMOp)request.MemoryOperation, (byte)request.NonVolatileKeyIndex, request.OperationData.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> LoadCryptoKeyFromNVM(LoadCryptoKeyFromNVMRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.LoadCryptoKeyFromNVM((byte)request.NonVolatileKeyIndex, (byte)request.VolatileKeyIndex, request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> SetCryptoID(SetCryptoIDRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.SetCryptoID(request.EncryptionID.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> SetCryptoInfo(SetCryptoInfoRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.SetCryptoInfo((SmallEarthTech.AntRadioInterface.EncryptionInfo)request.EncryptionParameter, request.ParameterData.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> SetCryptoKey(SetCryptoKeyRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.SetCryptoKey((byte)request.VolatileKeyIndex, request.EncryptionKey.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> SetCryptoRNGSeed(SetCryptoRNGSeedRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.SetCryptoRNGSeed(request.CryptoRNGSeed.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> SetCryptoUserInfo(SetCryptoUserInfoRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.SetCryptoUserInfo(request.UserInfoString.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+
+        override public Task<BoolValue> StoreCryptoKeyToNVM(StoreCryptoKeyToNVMRequest request, ServerCallContext context)
+        {
+            bool result = _crypto.StoreCryptoKeyToNVM((byte)request.NonVolatileKeyIndex, request.EncryptionKey.ToByteArray(), request.ResponseWaitTime);
+            return Task.FromResult(new BoolValue { Value = result });
+        }
+    }
+}
---
+++ Examples/MAUI-gRPC/AntGrpcShared/ClientServices/AntCryptoService.cs
@@ -0,0 +1,58 @@
+﻿using AntCryptoGrpcService;
+using SmallEarthTech.AntRadioInterface;
+
+namespace AntGrpcShared.ClientServices
+{
+    public partial class AntRadioService : ICrypto
+    {
+        private gRPCAntCrypto.gRPCAntCryptoClient? _crypto;
+
+        /// <inheritdoc/>
+        public bool CryptoKeyNVMOp(SmallEarthTech.AntRadioInterface.EncryptionNVMOp memoryOperation, byte nonVolatileKeyIndex, byte[] operationData, uint responseWaitTime = 0)
+        {
+            return _crypto!.CryptoKeyNVMOp(new CryptoKeyNVMOpRequest { MemoryOperation = (AntCryptoGrpcService.EncryptionNVMOp)memoryOperation, NonVolatileKeyIndex = nonVolatileKeyIndex, OperationData = Google.Protobuf.ByteString.CopyFrom(operationData), ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool LoadCryptoKeyFromNVM(byte nonVolatileKeyIndex, byte volatileKeyIndex, uint responseWaitTime = 0)
+        {
+            return _crypto!.LoadCryptoKeyFromNVM(new LoadCryptoKeyFromNVMRequest { NonVolatileKeyIndex = nonVolatileKeyIndex, VolatileKeyIndex = volatileKeyIndex, ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool SetCryptoID(byte[] encryptionID, uint responseWaitTime = 0)
+        {
+            return _crypto!.SetCryptoID(new SetCryptoIDRequest { EncryptionID = Google.Protobuf.ByteString.CopyFrom(encryptionID), ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool SetCryptoInfo(SmallEarthTech.AntRadioInterface.EncryptionInfo encryptionParameter, byte[] parameterData, uint responseWaitTime = 0)
+        {
+            return _crypto!.SetCryptoInfo(new SetCryptoInfoRequest { EncryptionParameter = (AntCryptoGrpcService.EncryptionInfo)encryptionParameter, ParameterData = Google.Protobuf.ByteString.CopyFrom(parameterData), ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool SetCryptoKey(byte volatileKeyIndex, byte[] encryptionKey, uint responseWaitTime = 0)
+        {
+            return _crypto!.SetCryptoKey(new SetCryptoKeyRequest { VolatileKeyIndex = volatileKeyIndex, EncryptionKey = Google.Protobuf.ByteString.CopyFrom(encryptionKey), ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool SetCryptoRNGSeed(byte[] cryptoRNGSeed, uint responseWaitTime = 0)
+        {
+            return _crypto!.SetCryptoRNGSeed(new SetCryptoRNGSeedRequest { CryptoRNGSeed = Google.Protobuf.ByteString.CopyFrom(cryptoRNGSeed), ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool SetCryptoUserInfo(byte[] userInfoString, uint responseWaitTime = 0)
+        {
+            return _crypto!.SetCryptoUserInfo(new SetCryptoUserInfoRequest { UserInfoString = Google.Protobuf.ByteString.CopyFrom(userInfoString), ResponseWaitTime = responseWaitTime }).Value;
+        }
+
+        /// <inheritdoc/>
+        public bool StoreCryptoKeyToNVM(byte nonVolatileKeyIndex, byte[] encryptionKey, uint responseWaitTime = 0)
+        {
+            return _crypto!.StoreCryptoKeyToNVM(new StoreCryptoKeyToNVMRequest { NonVolatileKeyIndex = nonVolatileKeyIndex, EncryptionKey = Google.Protobuf.ByteString.CopyFrom(encryptionKey), ResponseWaitTime = responseWaitTime }).Value;
+        }
+    }
+}
